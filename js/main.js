ac0cc8b19068a83cab7eb39cd575f04e0ead8040
pankowskitomasz <pankowski_tomasz@o2.pl>
2020-11-19 20:37:44 +0100

    finished frontend

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(".navbar-toggler-icon").click(function(){
+$(".navbar-toggler-icon").on("click",function(){
     $(".nav-menu").toggleClass("hidden");
 });    
+
+$("#index-s4-owl").owlCarousel({
+    loop:true,
+    responsive:{
+        0:{items:1},
+        576:{items:2},
+        768:{items:3},
+        992:{items:4}
+    }
+});
