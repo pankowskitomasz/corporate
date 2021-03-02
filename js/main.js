2fb12edc4d902a48af54ec25890f80180a632474
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-03-02 21:17:03 +0100

    frontend: modified and optimized

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ $(".navbar-toggler-icon").on("click",function(){
     $(".nav-menu").toggleClass("hidden");
 });    
 
-$("#index-s4-owl").owlCarousel({
-    loop:true,
-    responsive:{
-        0:{items:1},
-        576:{items:2},
-        768:{items:3},
-        992:{items:4}
-    }
-});
+if(document.querySelector("#index-s4-owl")){
+    $("#index-s4-owl").owlCarousel({
+        loop:true,
+        responsive:{
+            0:{items:1},
+            576:{items:2},
+            768:{items:3},
+            992:{items:4}
+        }
+    });
+}
